35123b4d1fa9f304ef2bd85713c96aa636e818c2
Unknown User <jumperkylian@outlook.com>
2024-02-25 23:00:03 +0100

    fix(self): Changing redis host

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const
     Redis = require('ioredis');
     spoofcustomstatus = require('./data/statuschanger.json')
     redis = new Redis({
-        host: '172.17.0.3',
+        host: '172.17.0.4',
         port: 6379,
     });
     fetch = require('node-fetch');
